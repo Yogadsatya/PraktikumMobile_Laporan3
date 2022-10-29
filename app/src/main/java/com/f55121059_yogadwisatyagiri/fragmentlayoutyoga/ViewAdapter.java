78ea99f51a7b268c435dac9fbe228a06db0c989d
Dianty Lujeng Rahayu <yogadsatya>
2022-10-29 13:44:37 +0800

    Initial commit

---
+++ app/src/main/java/com/f55121059_yogadwisatyagiri/fragmentlayoutyoga/ViewAdapter.java
@@ -0,0 +1,7 @@
+package com.f55121059_yogadwisatyagiri.fragmentlayoutyoga;
+
+import androidx.fragment.app.Fragment;
+
+public interface ViewAdapter {
+    void AddFragment(Fragment fragment, String Title);
+}
